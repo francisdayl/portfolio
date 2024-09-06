0130588d9933a1feb6a93ed28ac891c09c6a480f
David <david_yanez99@outlook.es>
2024-09-06 00:53:15 -0500

    progess project cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+export const content = [
+	'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	'./node_modules/flowbite/**/*.js'
+];
+export const theme = {
+	extend: {
+		keyframes: {
+			scale: {
+			  '0%': { transform: 'scale(1)' },
+			  '50%': { transform: 'scale(1.10)' },
+			  '100%': { transform: 'scale(1.20)' },
+			},
+		  },
+		  animation: {
+			scale: 'scale 0.3s ease forwards',
+		  },
 	},
-	plugins: [],
-}
+};
+export const plugins = [
+	require('flowbite/plugin')
+];
