7dd5daf86b4148dc716449df1e31fdfb0819281c
David <david_yanez99@outlook.es>
2024-09-17 18:20:57 -0500

    add custom paths

--- public/locales/en/projects.ts
+++ public/locales/en/projects.ts
@@ -1,28 +1,33 @@
 import { Angular, AstroIcon, Aws, Bootstrap, Django, Docker, ExpressIcon, FastApi, 
     Figma, Flask, Flutter, Git, IconSkill, Java, Javascript, Mongo, MySql, NodeJs, PostgreSQL, 
     Postman, Python, React, Rive, Selenium, SQLite, Tailwind, Typescript } 
-    from "../../../src/components/icons/index"
+    from "@src/components/icons/index"
+
+import GenHoraCapture from "@public/assets/captures/capture_genhora.webp";
+import MyPdfToolkitCapture from "@public/assets/captures/capture_pdf.webp";
+import MyPortfolioCapture from "@public/assets/captures/catpure_portfolio.webp";
+import TechstoreCapture from "@public/assets/captures/capture_techstore.png";
 
 export const englishProjects = [
-    {image:"assets/captures/capture_pdf.webp", title:"My PDF Toolkit", 
+    {image:MyPdfToolkitCapture.src, title:"My PDF Toolkit", 
         description:"Application for PDF manipulation", 
         website:"https://mmgkxuq3hr.us-east-1.awsapprunner.com/", 
         repository:"https://github.com/francisdayl/my_pdf_toolkit",
         technologies:[Python,Flask,Aws,Docker,Bootstrap,Git]
     },
-    {image:"assets/captures/capture_genhora.webp", title:"GenHora", 
+    {image:GenHoraCapture.src, title:"GenHora", 
         description:"Program that allows to generate all possible schedules based on student/user preferences. The schedules are saved on excel files", 
         website:"", 
         repository:"https://github.com/francisdayl/GenHora",
         technologies:[Python,Selenium,Git]
     },
-    {image:"assets/captures/capture_techstore.png", title:"TechStore", 
+    {image:TechstoreCapture.src, title:"TechStore", 
         description:"Technical test consisting on a Full Stack application with CRUD operations.", 
         website:"", 
         repository:"https://github.com/francisdayl/prueba_tecnica_royaltic",
         technologies:[Angular,Django,MySql,Python,Typescript,Docker,Git]
     },
-    {image:"assets/captures/catpure_portfolio.webp", title:"Portfolio", 
+    {image:MyPortfolioCapture.src, title:"Portfolio", 
         description:"Web Portfolio showcasing profile and skillset", 
         website:"", 
         repository:"https://github.com/francisdayl/portfolio",
--- public/locales/es/projects.ts
+++ public/locales/es/projects.ts
@@ -1,28 +1,34 @@
 import { Angular, AstroIcon, Aws, Bootstrap, Django, Docker, ExpressIcon, FastApi, 
     Figma, Flask, Flutter, Git, IconSkill, Java, Javascript, Mongo, MySql, NodeJs, PostgreSQL, 
     Postman, Python, React, Rive, Selenium, SQLite, Tailwind, Typescript } 
-    from "../../../src/components/icons/index"
+    from "@src/components/icons/index"
+
+import GenHoraCapture from "@public/assets/captures/capture_genhora.webp";
+import MyPdfToolkitCapture from "@public/assets/captures/capture_pdf.webp";
+import MyPortfolioCapture from "@public/assets/captures/catpure_portfolio.webp";
+import TechstoreCapture from "@public/assets/captures/capture_techstore.png";
 
 export const spanishProjects = [
-    {image:"assets/captures/capture_pdf.webp", title:"My PDF Toolkit", 
+    {image:MyPdfToolkitCapture.src,
+        title:"My PDF Toolkit", 
         description:"Aplicación web para manipular PDF", 
         website:"https://mmgkxuq3hr.us-east-1.awsapprunner.com/", 
         repository:"https://github.com/francisdayl/my_pdf_toolkit",
         technologies:[Python,Flask,Aws,Docker,Bootstrap,Git]
     },
-    {image:"assets/captures/capture_genhora.webp", title:"GenHora", 
+    {image:GenHoraCapture.src, title:"GenHora", 
         description:"Programa que permite generar todos los horarios posibles basados en las preferencias del estudiante/usuario. Los horarios son guardados en archivos excel.", 
         website:"", 
         repository:"https://github.com/francisdayl/GenHora",
         technologies:[Python,Selenium,Git]
     },
-    {image:"assets/captures/capture_techstore.png", title:"TechStore", 
+    {image:TechstoreCapture.src, title:"TechStore", 
         description:"Prueba técnica que consiste en un aplicación web Full Stack con operaciones CRUD.", 
         website:"", 
         repository:"https://github.com/francisdayl/prueba_tecnica_royaltic",
         technologies:[Angular,Django,MySql,Python,Typescript,Docker,Git]
     },
-    {image:"assets/captures/catpure_portfolio.webp", title:"Portfolio", 
+    {image:MyPortfolioCapture.src, title:"Portfolio", 
         description:"Portafolio web para mostrar mi perfil y habilidades", 
         website:"", 
         repository:"https://github.com/francisdayl/portfolio",
